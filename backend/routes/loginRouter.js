2b0fa0ef45adbd6ffd31241a39534d158b7b503d
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2025-08-01 11:45:20 -0400

    add login routes

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -1,5 +1,9 @@
 import express from "express";
+import * as loginController from "../controllers/loginController.js";
 
 const loginRouter = express.Router();
+
+loginRouter.post("/login", loginController.loginUser);
+loginRouter.post("/register", loginController.registerUser);
 
 export default loginRouter;
